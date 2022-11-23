d444fd36541e485bde792c3f8ac67ec0d873a482
Sailerh <sailer@medien.at>
2022-11-23 13:38:49 +0100

    End of section 16

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -19,7 +19,11 @@ namespace API.Helpers
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>())
                 ;
 
-            CreateMap<Address, AddressDto>().ReverseMap();           
+            CreateMap<Address, AddressDto>().ReverseMap();
+
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+
+            CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
